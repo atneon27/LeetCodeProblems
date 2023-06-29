2cf45fae4a052233110ffa0d0513358a95dd5606
atneon27 <132840385+atneon27@users.noreply.github.com>
2023-06-29 23:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (92.46%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+#include<cctype>
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (!isalpha(s[i]) && !isdigit(s[i]) && i <= j) {
+                i++;
+            }
+            while (!isalpha(s[j]) && !isdigit(s[j]) && i <= j) {
+                j--;
+            }
+            if (i >= j) {
+                break;
+            }
+            if (toupper(s[i]) != toupper(s[j])) {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
