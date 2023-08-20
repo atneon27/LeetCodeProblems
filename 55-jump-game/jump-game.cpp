5533fd013faeb70c5a758357376fb5d9d558da19
atneon27 <132840385+atneon27@users.noreply.github.com>
2023-08-20 22:51:35 +0530

    Time: 40 ms (95.61%) | Memory: 48.4 MB (74.70%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int rechable=0;//jaha tak ja sakte ho 
-        for(int i=0;i<n;i++)//rechable < n nahi rechable < i hoga
+        int rechable=0; 
+        for(int i=0;i<n;i++)
         {
             if(rechable<i){
                 return false;
